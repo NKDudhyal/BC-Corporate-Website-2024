26a16d6fb62ce038c0ac783a19ea6ce3910a35fd
Naveen Dudhyal <dudhyalnaveen22@gmail.com>
2024-12-27 13:57:14 +0530

    2nd report for homepage

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,7 @@ module.exports = defineConfig({
     "reporterOptions": {
       "reportDir": "cypress/reports",
       "overwrite": false,
+      "inlineAssets": true,
       "html": true,
       "json": false
     }
